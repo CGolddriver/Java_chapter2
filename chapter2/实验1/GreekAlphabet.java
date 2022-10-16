75a7c9c1d1421597f189b2683ce78e8daa806958
脱碳蔗糖 <94093893+CGolddriver@users.noreply.github.com>
2022-10-16 23:21:09 +0800

    Add files via upload

---
+++ chapter2/实验1/GreekAlphabet.java
@@ -0,0 +1,12 @@
+class Tom {
+    int weight  = 10;
+    void Tom(){
+        weight = 18;
+    }
+}
+public class E {
+    public static void main(String args[]) {
+       Tom cat = new Tom();
+       System.out.println(cat.weight); //¡¾´úÂë¡¿
+    }
+}
